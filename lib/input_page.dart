b4e3c12fb549337ed6bae0d9bc84554c7cc22d12
Manohar <manoharacharya63@gmail.com>
2023-03-07 16:24:59 +0530

    4th Refractoring the Container & making it immutable by adding final

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -20,62 +20,48 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Color(0xff1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                  child: ReusableCard(Color(0xFF1D1E33)),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Color(0xff1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                  child: ReusableCard(Color(0xFF1D1E33)),
                 ),
               ],
             ),
           ),
           Expanded(
             child: Expanded(
-              child: Container(
-                margin: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Color(0xff1D1E33),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
+              child: ReusableCard(Color(0xFF1D1E33)),
             ),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Color(0xff1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                  child: ReusableCard(Color(0xFF1D1E33)),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Color(0xff1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                  child: ReusableCard(Color(0xFF1D1E33)),
                 ),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+//Creating a ReuableCard
+class ReusableCard extends StatelessWidget {
+  ReusableCard(@required this.colour); //constructor
+  final Color colour; //proprty
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
