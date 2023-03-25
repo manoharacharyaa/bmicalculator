65a7e4388f8e1e65275a7636d3c75dae82f2afae
Raju Potharaju <raju@Rajus-MacBook-Pro.local>
2023-03-25 17:20:45 +0530

    Fixed setState issue

---
+++ lib/widgets/build_round_btn.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+  final IconData icon;
+  final Function handler;
+
+  const RoundIconButton({super.key, required this.icon, required this.handler});
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () => handler(),
+      child: Icon(icon),
+      constraints: BoxConstraints.tightFor(
+        width: 56,
+        height: 56,
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
+    );
+  }
+}
